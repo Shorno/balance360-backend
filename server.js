7d22dad3ad88278e4f4dd30846fef2e3519b1f82
Shorno <roy15-6141@s.diu.edu.bd>
2025-01-17 01:11:47 +0600

    update domain url

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173", "https://balance360.vercel.app/"],
+        origin: ["http://localhost:5173", "https://balance360.vercel.app"],
         credentials: true,
     }
 ));
