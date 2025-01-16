c0101069b363768db85616069f99094f677054ac
Shorno <roy15-6141@s.diu.edu.bd>
2025-01-17 00:45:46 +0600

    add cookie parser

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import cookieParser from "cookie-parser";
 import morgan from 'morgan';
+
+
 import connectDB from "./config/database.js";
-
-
 import userRoutes from "./routes/userRoutes.js"
 import trainerRoutes from "./routes/trainerRoutes.js"
 
@@ -23,6 +24,8 @@ app.use(cors(
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser())
+
 
 app.get('/', (req, res) => {
     res.send('Hello, this api server for Fit Fam Project');
